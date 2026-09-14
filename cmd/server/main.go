e4aa9b2888384cdccf019af236736dd183de40e0
RemEx <74838147+Rem7474@users.noreply.github.com>
2026-09-14 07:51:42 +0200

    chore(release): bump version to v1.5.0

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -101,7 +101,7 @@ func main() {
 		MaxAge:           300,
 	}))
 
-	const AppVersion = "1.4.0"
+	const AppVersion = "1.5.0"
 
 	// Public Health Check Endpoint
 	healthHandler := func(w http.ResponseWriter, r *http.Request) {
